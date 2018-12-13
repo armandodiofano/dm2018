52b96886c04e7e40c64527e7b656c818f382b827
Danilo <danilo@novedge.com>
2018-12-13 16:32:44 +0100

    DBSCAN v2

--- dbscan.py
+++ dbscan.py
@@ -19,23 +19,12 @@ dist = squareform(dist)
 
 
 k = 280
-'''
-kth_distances = list()
-for d in dist:
-    index_kth_distance = numpy.argsort(d)[k]
-    kth_distances.append(d[index_kth_distance])
-
-plt.plot(range(0, len(kth_distances)), sorted(kth_distances))
-plt.ylabel('dist from %sth neighbor' % k)
-plt.xlabel('sorted distances')
-
-plt.show()
-'''
-for i in range(50,400):
-    dbscan = DBSCAN(eps=0.32, min_samples=i)
-    dbscan.fit(X)
-
-    print("%s" %i, numpy.unique(dbscan.labels_, return_counts=True))
+for i in range(400,800, 25):
+    for e in numpy.arange(0.27, 0.37, 0.005):
+        dbscan = DBSCAN(eps=e, min_samples=i)
+        dbscan.fit(X)
+        print(i, e , numpy.unique(dbscan.labels_, return_counts=True))
+        
 #print "silhoutte_score:", silhouette_score(kmeans_df, kmeans.labels_)
 
 
